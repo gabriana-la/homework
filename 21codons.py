bfab1ff5d65721284980a90adc03866f711cf6d8
gabriana-la <gnla@ucdavis.edu>
2023-01-26 21:44:46 -0800

    new

--- 21codons.py
+++ 21codons.py
@@ -6,15 +6,9 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
-print(dna[0:2])
-print(dna[3:5])
-print(dna[6:8])
-print(dna[9:10])
-print(dna[11:13])
-print(dna[14:16])
-print(dna[17:19])
-print(dna[20:22])
-print(dna[23:25])
+k = 3
+for i in range(0, len(dna)- k + 3, k):
+	print(dna[i:i+k])
 
 """
 python3 21codons.py
