d89b07bf46f2f5b94c088527dc2a5bc4b2c7249c
gabriana-la <gnla@ucdavis.edu>
2023-01-26 21:27:29 -0800

    new

--- 21codons.py
+++ 21codons.py
@@ -6,6 +6,15 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
+print(dna[0:2])
+print(dna[3:5])
+print(dna[6:8])
+print(dna[9:10])
+print(dna[11:13])
+print(dna[14:16])
+print(dna[17:19])
+print(dna[20:22])
+print(dna[23:25])
 
 """
 python3 21codons.py
