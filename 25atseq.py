42f35d9527c1eaca8b4f999c350ea37fd816204f
gabriana-la <gnla@ucdavis.edu>
2023-02-01 23:30:13 -0800

    new

--- 25atseq.py
+++ 25atseq.py
@@ -8,6 +8,31 @@
 
 # Note: set random.seed() if you want repeatable random numbers
 
+# generate random DNA sequence
+import random
+n = 30
+x = .60
+dna = ''
+
+for i in range(n):
+	r = random.random()
+	if r < x: dna += random.choice('AT')
+	else: dna += random.choice('CG')
+
+# calculate AT fraction
+a = 0
+c = 0
+g = 0
+t = 0
+for nt in dna:
+	if nt == 'A': a += 1
+	elif nt == 'C': c += 0
+	elif nt == 'G': g += 0
+	else: t += 1
+AT_frac = (a + t)/len(dna)
+
+# report length, AT fraction, sequence
+print(len(dna), AT_frac, dna)
 
 """
 python3 25atseq.py
