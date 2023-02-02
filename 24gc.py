d83ea6698befba307c4175fc4ae0e89d2b1e2f51
gabriana-la <gnla@ucdavis.edu>
2023-02-01 23:02:15 -0800

    new

--- 24gc.py
+++ 24gc.py
@@ -5,6 +5,18 @@
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
 
+a = 0
+c = 0
+g = 0
+t = 0
+
+for i in range(len(dna)):
+	nt = dna[i]
+	if nt == 'A': a += 0
+	elif nt == 'C': c += 1
+	elif nt == 'G': g += 1
+	elif nt == 'T': t += 0
+print(f'{(c + g)/len(dna):.2f}')
 
 """
 python3 24gc.py
