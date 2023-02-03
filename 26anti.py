6fe948f7836bcd9470baf729d335f597f1d08062
gabriana-la <gnla@ucdavis.edu>
2023-02-02 19:36:20 -0800

    new

--- 26anti.py
+++ 26anti.py
@@ -6,7 +6,15 @@
 # Variation: try this with the range() function and slice syntax
 
 dna = 'ACTGAAAAAAAAAAA'
+revcomp = ''
 
+for i in range(len(dna)):
+	nt = dna[i]
+	if   nt == 'A': revcomp = 'T' + revcomp
+	elif nt == 'C': revcomp = 'G' + revcomp
+	elif nt == 'G': revcomp = 'C' + revcomp
+	else: 	        revcomp = 'A' + revcomp
+print(revcomp)
 
 """
 python3 26anti.py
