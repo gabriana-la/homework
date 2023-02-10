507695d70bb439a7120d72dd9cc0a9353c1eadcc
gabriana-la <gnla@ucdavis.edu>
2023-02-09 20:56:02 -0800

    new

--- 30stats.py
+++ 30stats.py
@@ -7,6 +7,41 @@
 
 # Note: you are not allowed to import any library except sys
 
+import sys
+
+# print(sys.argv)
+
+vals = []
+for thing in sys.argv[1:]:
+	vals.append(float(thing))
+# print(vals)
+
+print('Count:', len(vals))
+
+print('Minimum:', min(vals))
+
+print('Maximum:', max(vals))
+
+mean = sum(vals)/len(vals)
+print('Mean:', f'{mean:.3f}')
+
+sum = 0
+for val in vals:
+	sum += (val - mean) ** 2
+variance = sum / len(vals)
+std_dev = variance ** 0.5
+	
+print('Std. dev:', f'{std_dev:.3f}')
+
+vals.sort()
+mid = len(vals) // 2
+median = None
+
+if len(vals) % 2 == 0:
+	median = (vals[mid] + vals[mid - 1]) / 2
+else: 
+	median = vals[mid]
+print('Median:', f'{median:.3f}')
 
 """
 python3 30stats.py 3 1 4 1 5
