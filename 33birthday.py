280d771e45c25a165a6e36c65525c8d03203aa92
gabriana-la <gnla@ucdavis.edu>
2023-02-14 20:35:27 -0800

    new

--- 33birthday.py
+++ 33birthday.py
@@ -11,7 +11,6 @@
 # Variation: try making the birthdays a list
 
 import random
-random.seed(1)
 import sys
 
 num_days = int(sys.argv[1])
@@ -19,18 +18,69 @@ num_days = int(sys.argv[1])
 num_people = int(sys.argv[2])
 print(num_days, num_people)
 
+trials = 10000
+shared = 0
 
-calendar = [0] * num_days
-print(calendar)
 
-birthday = random.randint(0, num_days)
-print(birthday)
+# fill empty calendar, check as you go, fastest but takes up more space
+for trial in range(trials):
+	calendar = [0] * num_days	
+	for i in range(num_people):
+		birthday = random.randint(0, num_days - 1)
+		if calendar[birthday] == 1:
+			shared += 1
+			break
+		calendar[birthday] += 1
+print(shared/trials)
+
+
+"""
+# fill empty calendar, check after, slow
+for trial in range(trials):
+	calendar = [0] * num_days
+	found = False
+	for i in range(num_people):
+		birthday = random.randint(0, num_days - 1)
+		calendar[birthday] += 1
 	
-for i in calendar:
-	if i == birthday:
-		calendar[i] += 1
-print(calendar)
-	
+	for bday in calendar:
+		 if bday > 1:
+		 	found = True
+		 	break
+	if found:
+		 shared += 1
+print(shared/trials)
+"""
+
+"""
+# create birthdays, check as you go, 2nd fastest but takes up less space
+for trial in range(trials):
+	birthdays = []
+	for i in range(num_people):
+		birthday = random.randint(0, num_days - 1)
+		if birthday in birthdays:
+			shared += 1
+			break
+		birthdays.append(birthday)
+print(shared/trials)
+"""
+
+"""
+# create birthdays, compare against each (pairwise comparison), slowest
+for trial in range(trials):
+	birthdays = []	
+	for i in range(num_people):
+		birthdays.append(random.randint(0, num_days - 1))
+		found = False
+		for j in range(i + 1, num_people):
+			if birthdays[i] == birthdays[j]:
+				found = True
+				break
+		if found:
+			shared += 1
+			break
+print(shared/trials)
+"""
 
 """
 python3 33birthday.py 365 23
