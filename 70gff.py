f11b104023fb2658cb749aa43194e84bebfbe2d4
gabriana-la <gnla@ucdavis.edu>
2023-03-15 20:05:06 -0700

    new

--- 70gff.py
+++ 70gff.py
@@ -4,6 +4,38 @@
 # Use the E. coli genome gff
 # Your code should mimic the output below
 
+import sys
+import gzip
+import re
+import json
+
+filename = sys.argv[1]
+
+genome = []
+
+with gzip.open(filename, 'rt') as fp:
+	for line in fp:
+		if line.startswith('#'): continue
+		for match in re.finditer('RefSeq\s+gene', line):
+			info = {}
+			name = re.search('Name=(\w+)', line)
+			gene = str(name.group(1))
+			
+			coordinates = re.search('(\d+)\s+(\d+)', line)
+			beg = int(coordinates.group(1))
+			end = int(coordinates.group(2))
+			
+			sense = re.search('\.\s+(.)\s+\.', line)
+			strand = sense.group(1)
+			
+			info['gene'] = gene
+			info['beg'] = beg
+			info['end'] = end
+			info['strand'] = strand
+			genome.append(info)
+
+print(json.dumps(genome, indent=4))
+
 
 """
 python3 70gff.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gff.gz
